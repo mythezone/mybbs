ecfec6403cd0775d824a4d9c530c4693cc6012e3
mythezone <125616515@qq.com>
2021-06-28 19:48:58 +0800

    first commit

---
+++ index.php
@@ -0,0 +1,6 @@
+<?php 
+$TITLE="Let's Talk About Everything Interesting!~";
+require_once "templates/header.php"; ?>
+    <h1>New: Hello, world!</h1>
+<?php require_once "templates/footer.php"; ?>
+
